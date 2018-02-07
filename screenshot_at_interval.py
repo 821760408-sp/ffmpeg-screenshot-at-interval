29a2730304a3214be8f6f7164e0cf742dcaea09e
821760408-sp <821760408.sp@gmail.com>
2018-02-07 18:08:30 -0500

    added *start time* command-line argument

--- screenshot_at_interval.py
+++ screenshot_at_interval.py
@@ -8,8 +8,9 @@ import time
 parser = argparse.ArgumentParser(description='Script to extract either a number of {n} screenshots from a video, or extract an indefinite amount of screenshots at an interval of {i} seconds.')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-n', dest='num', type=int, help='specify the number of screenshots to extract from video.')
-group.add_argument('-s', dest='interval', type=int, help='specify the interval of screenshots to extract from video.')
-parser.add_argument('-i', dest='input', required=True, help='specify input video file.')
+group.add_argument('-i', dest='interval', type=int, help='specify the interval of screenshots to extract from video.')
+parser.add_argument('-s', dest='start_time', type=float, default=0.0, help='specify the starting time of video extracting.')
+parser.add_argument('-f', dest='input', required=True, help='specify input video file.')
 args = parser.parse_args()
 
 
@@ -20,9 +21,10 @@ def get_video_duration(filename):
 
 
 if __name__=='__main__':
+    screenshot_num = args.num
     interval = args.interval
+    start_time_in_sec = args.start_time
     filename = args.input
-    ss_num = None
     duration = get_video_duration(filename)[0].split(' ')[3].strip(',')
 
     duration = time.strptime(duration[:-3], '%H:%M:%S')
@@ -30,19 +32,17 @@ if __name__=='__main__':
             minutes=duration.tm_min,
             seconds=duration.tm_sec).total_seconds()  # in seconds
     if not interval:
-        ss_num = args.num  # number of screenshot
-        interval = duration // ss_num
+        interval = (duration - start_time_in_sec) // screenshot_num
 
-    if not ss_num:
-        ss_num = int(duration // interval)
+    if not screenshot_num:
+        screenshot_num = int((duration - start_time_in_sec) / interval)
 
-    start_time_in_sec = 0
     i = 0
-    times_in_sec = [start_time_in_sec + interval * i for i in range(ss_num)]
+    times_in_sec = [start_time_in_sec + interval * i for i in range(screenshot_num)]
 
     timestamps = [time.strftime('%H:%M:%S', time.gmtime(x)) for x in times_in_sec]
 
     for i in range(len(timestamps)):
-        with subprocess.Popen(['ffmpeg', '-ss', timestamps[i], '-i', filename, '-vframes', '1', 'out{0:02}.png'.format(i)]) as proc:
+        with subprocess.Popen(['ffmpeg', '-ss', timestamps[i], '-i', filename, '-frames:v', '1', 'out{0:04}.png'.format(i)]) as proc:
             pass
 
